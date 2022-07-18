a7cde6c088f30ce4c9f166a02aedeb9fcc7c35ae
Rahul Bharuka <rahuldev.cotocus@gmail.com>
2022-07-19 00:34:15 +0530

    updated

--- src/Pages/Post.js
+++ src/Pages/Post.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams, NavLink } from "react-router-dom";
+import Loader from "../components/Loader";
 
 /*
     On clicking link in PostPreview component navigate to /post/:id, which shows the Post Page.
@@ -14,14 +16,43 @@ import React from "react";
             <li> <code>p</code> with class <code>post-author</code> and text as <code>{user.name}</code> (use name property from user object)</li>
             <li> <code>NavLink</code> to <code>/</code> with text as <code>Back to Home</code> which takes to Index page </li>
         </ul>
-
  */
 
 export const Post = () => {
+  const { id } = useParams();
+  const [data, setData] = useState(null);
+  const [authorName, setAuthor] = useState(null);
+  const [isLoading, setLoading] = useState(true);
 
-    return (
-        <div id="post">
+  const loadData = () => {
+    setLoading(true);
+    fetch("https://jsonplaceholder.typicode.com/posts/" + id)
+      .then((response) => response.json())
+      .then((json) => {
+        setData(json);
+        fetch("https://jsonplaceholder.typicode.com/users/" + json.userId)
+          .then((response) => response.json())
+          .then((author) => {
+            setAuthor(author.name);
+          })
+          .catch((err) => console.log(err));
+        setLoading(false);
+      })
+      .catch((err) => console.log(err));
+  };
 
-        </div>
-    )
-}
+  useEffect(() => {
+    loadData();
+  }, []);
+  return isLoading ? (
+    <Loader />
+  ) : (
+    <div id="post">
+      <h1 className="post-id">Post id:- {data.id}</h1>
+      <h2 className="post-title">{data.title}</h2>
+      <p className="post-body">{data.body}</p>
+      <p className="post-author">{authorName}</p>
+      <NavLink to="/">Back Home</NavLink>
+    </div>
+  );
+};
